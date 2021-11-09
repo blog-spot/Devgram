51227fcbb9e8952451b0bcaddd3da1fbbecc7dbb
Udhay <57910021+code123841@users.noreply.github.com>
2021-11-09 20:03:55 +0530

    setting up app.ts

--- app.ts
+++ app.ts
@@ -1,1 +1,5 @@
-import express
+import express, {Request,Response,Application} from 'express';
+import * as fs from 'fs';
+var bodyParser = require('body-parser');
+const app:Application = express();
+const PORT = process.env.PORT || 3000;
