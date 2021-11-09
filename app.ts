40a16e14684186fc88fbb048ca3cdfa622b3874c
Udhay <57910021+code123841@users.noreply.github.com>
2021-11-09 20:05:06 +0530

    basic express app

--- app.ts
+++ app.ts
@@ -3,3 +3,11 @@ import * as fs from 'fs';
 var bodyParser = require('body-parser');
 const app:Application = express();
 const PORT = process.env.PORT || 3000;
+
+app.get('/' , (req: Request, res: Response) => {
+    res.send("hello")
+})
+
+app.listen(PORT, ():void => {
+    console.log(`Server Running here ⚡  https://localhost:${PORT}`);
+  });
