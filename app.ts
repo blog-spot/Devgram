f320b43d50eac44e1daf807ffd06b07637b7140a
Udhay <57910021+code123841@users.noreply.github.com>
2021-11-10 19:32:35 +0530

    connced to the online db

--- app.ts
+++ app.ts
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 const app:Application = express();
 const PORT = process.env.PORT || 3000;
 const axios = require('axios');
+var sql = require("mssql");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -16,6 +17,41 @@ app.use('/css', express.static(__dirname + 'public/css'))
 app.use('/fonts' , express.static(__dirname + 'public/fonts'))
 app.use('/img' , express.static(__dirname + 'public/img'))
 app.use('/bootstrap' , express.static(__dirname + 'public/bootstrap'))
+
+// mysql test connection
+
+var dbConfig = {
+    server: "dev-gram.database.windows.net", // Use your SQL server name
+    database: "users-api", // Database to connect to
+    user: "Udhay", // Use your username
+    password: "cisco123!@#", // Use your password
+    port: 1433,
+    // Since we're on Windows Azure, we need to set the following options
+    options: {
+          encrypt: true
+      }
+   };
+
+   function Conncet(){
+       
+ var conn = new sql.ConnectionPool(dbConfig);
+
+ conn.connect(
+     function (err:any) { 
+     if (err) { 
+         console.log("!!! Cannot connect !!! Error:");
+         throw err;
+     }
+     else
+     {
+        console.log("Connection established.");
+     }
+ });
+ 
+
+   }
+
+Conncet();
 
 
 
